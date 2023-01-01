ddccdd83e17c6f152458165546d2c96fdf466b19
blane thompson <blane1245@hotmail.co.uk>
2023-01-01 11:40:53 +0000

    initial commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/TheRockettek/yt-dlcc
+
+go 1.18
+
+require (
+	github.com/gorilla/websocket v1.5.0
+	github.com/json-iterator/go v1.1.12
+)
+
+require (
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"os/exec"
+
+	"github.com/gorilla/websocket"
+	jsoniter "github.com/json-iterator/go"
+)
+
+var upgrader = websocket.Upgrader{}
+
+type query struct {
+	URL string `json:"url"`
+}
+
+func validateURL(str string) bool {
+	_, err := url.Parse(str)
+	return err == nil
+}
+
+func main() {
+	host := flag.String("host", os.Getenv("HOST"), "Host")
+	flag.Parse()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Print("upgrade failed: ", err)
+			return
+		}
+
+		defer conn.Close()
+
+		// Continuously read and write message
+		for {
+			mt, message, err := conn.ReadMessage()
+			if err != nil {
+				log.Println("read failed:", err)
+				break
+			}
+
+			var query query
+			err = jsoniter.Unmarshal(message, query)
+			if err != nil {
+				log.Println("read failed:", err)
+				break
+			}
+
+			if validateURL(query.URL) {
+				log.Println("Query for " + query.URL)
+				command := fmt.Sprintf("yt-dlp %s -o - | ffmpeg -i pipe: -c:a dfpwm -f wav pipe:", query.URL)
+
+				cmd := exec.Command("bash", "-c", command)
+				stdout, err := cmd.StdoutPipe()
+				cmd.Stderr = cmd.Stdout
+				if err != nil {
+					log.Println("stdout failed:", err)
+					break
+				}
+				if err = cmd.Start(); err != nil {
+					log.Println("start failed:", err)
+					break
+				}
+
+				for {
+					tmp := make([]byte, 65536)
+					_, err := stdout.Read(tmp)
+					if err != nil {
+						log.Println("read stdout failed:", err)
+						break
+					}
+
+					err = conn.WriteMessage(mt, tmp)
+					if err != nil {
+						log.Println("write failed:", err)
+						break
+					}
+				}
+			}
+		}
+	})
+
+	log.Println("Serving at " + *host)
+	http.ListenAndServe(*host, nil)
+}
